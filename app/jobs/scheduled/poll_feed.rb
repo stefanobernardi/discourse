5676debf36542ca7fe77a207e68e310bda2f6fe4
Stefano Bernardi <hello@stefanobernardi.com>
2014-01-12 11:44:19 -0800

    Fixing bad merge

--- app/jobs/scheduled/poll_feed.rb
+++ app/jobs/scheduled/poll_feed.rb
@@ -4,14 +4,7 @@
 require 'digest/sha1'
 require_dependency 'post_creator'
 require_dependency 'post_revisor'
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 require 'open-uri'
->>>>>>> upstream/master
-=======
-require 'open-uri'
->>>>>>> upstream/master
 
 module Jobs
   class PollFeed < Jobs::Scheduled
@@ -32,32 +25,6 @@ module Jobs
       user = User.where(username_lower: SiteSetting.embed_by_username.downcase).first
       return if user.blank?
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-      fetch_opts = {}
-
-      last_modified = $redis.get(feed_key)
-      if last_modified.present?
-        fetch_opts[:if_modified_since] = Time.parse(last_modified)
-      end
-
-      require 'feedzirra'
-      feed = Feedzirra::Feed.fetch_and_parse(SiteSetting.feed_polling_url, fetch_opts)
-
-      if feed.respond_to?(:entries)
-        feed.entries.each do |e|
-          url = e.url
-          url = e.id if url.blank? || url !~ /^https?\:\/\//
-          TopicEmbed.import(user, url, e.title, e.content)
-        end
-
-        # Store last modified for faster requests from feeds
-        $redis.set(feed_key, feed.last_modified)
-
-        false
-=======
-=======
->>>>>>> upstream/master
       require 'simple-rss'
       rss = SimpleRSS.parse open(SiteSetting.feed_polling_url)
 
@@ -69,10 +36,6 @@ module Jobs
         if content
           TopicEmbed.import(user, url, i.title, CGI.unescapeHTML(content.scrub))
         end
-<<<<<<< HEAD
->>>>>>> upstream/master
-=======
->>>>>>> upstream/master
       end
     end
 
